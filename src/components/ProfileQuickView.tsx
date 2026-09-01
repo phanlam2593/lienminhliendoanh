620d9bae8d5bd4a8c41ef8104bb14d09a336e6a8
phanlam2593 <phanlam2593@gmail.com>
2026-09-02 00:14:28 +0700

    Fix import statement formatting in ProfileQuickView

--- src/components/ProfileQuickView.tsx
+++ src/components/ProfileQuickView.tsx
@@ -1,4 +1,4 @@
-…import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { Mail, MessageCircle, Phone, UserCheck, UserPlus } from "lucide-react";
